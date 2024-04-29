65b8a61e3cb3b4dda60f18671a93ca5430307ee3
“Rahul <rahulzade2112@gmail.com>
2024-04-29 14:18:26 +0530

    Signup frontend  page ready

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import './App.css'
 import Signin from './Components/Signin/Signin'
+import Signup from './Components/Signup/Signup'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,8 @@ function App() {
   return (
     <>
 <div>
-<Signin/>
+<Signup/>
+{/* <Signin/> */}
 </div>
     
     </>
